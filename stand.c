5a25fadf0b96d75adebab5b58f2a5cee019c1ba6
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-11-29 06:18:12 +0100

    refactor into separate functions

--- stand.c
+++ stand.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int parse_int(const char *s) {
     int n = 0;
@@ -14,20 +15,27 @@ int parse_int(const char *s) {
     return n;
 }
 
-int main(int argc, char **argv) {
-    time_t tstart;
-    time_t tnow;
-    time(&tstart);
-
-    int interval = 3600; // default interval: 1 hour
-    if (argc > 1) {
-        interval = parse_int(argv[1]);
+int notify() {
+    // send notification
+    FILE *pout = popen("notify-send -u critical --app-name=\"Stand!\" --action=1=\"I'm up!\" \"Stand\" --action=2=\"Ignore for 2 hours\" --action=24=\"Ignore rest of day\" \"It's time to stand up!\"", "r");
+    if (pout == NULL) {
+       return 1;
     }
 
-    time_t tnext = tstart + interval;
+    // read stdout
+    char s[32] = "";
+    fread(s, 1, 30, pout);
+    int status = pclose(pout);
+    if (status != EXIT_SUCCESS) {
+        return 1;
+    }
 
+    return parse_int(s);
+}
+
+void print_startup_message(time_t tnext, int interval) {
     struct tm *t = localtime(&tnext);
-    char s[32];
+    char s[32] = "";
     strftime(s, 32, "%l:%M %p", t);
 
     // print some info to stdout
@@ -35,27 +43,36 @@ int main(int argc, char **argv) {
     printf("-  Interval: %d seconds\n", interval);
     printf("-  Next reminder: %s\n", s);
     fflush(stdout);
+}
+
+int main(int argc, char **argv) {
+    time_t tstart;
+    time_t tnow;
+    time_t tnext;
+    int action;
+    int interval;
+
+    if (argc > 1) {
+        interval = parse_int(argv[1]);
+    } else {
+        interval = 3600; // default interval: 1 hour
+    }
+
+    time(&tstart);
+    tnext = tstart + interval;
+
+    print_startup_message(tnext, interval);
 
     while (1) {
         sleep(1);
         time(&tnow);
 
         if (tnow > tnext) {
-            // send notification
-            FILE *pout = popen("notify-send -u critical --app-name=\"Stand!\" --action=1=\"I'm up!\" \"Stand\" --action=2=\"Ignore for 2 hours\" --action=24=\"Ignore rest of day\" \"It's time to stand up!\"", "r");
-            if (pout == NULL) {
-                time(&tnow);
-                tnext = tnow + interval;
-                continue;
-            }
-
-            // read stdout
-            fread(s, 1, 30, pout);
-            pclose(pout);
+            action = notify();
 
             // determine next notification time
             time(&tnow);
-            tnext = tnow + (parse_int(s) * interval);
+            tnext = tnow + (action * interval);
         }
     }
 
